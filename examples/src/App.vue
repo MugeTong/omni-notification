5c3a42e32cb34aba2a24e72dccc56e0c9810a102
MUGE_TONG <here5320@gmail.com>
2025-02-12 10:35:24 +0800

    add examples like a vue project

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,18 +1,59 @@
 <script setup>
 import { inject } from 'vue';
+import TitleBar from './componets/TitleBar.vue';
 
 const notify = inject('notify');
-console.log(notify)
+console.log(notify);
 
 </script>
 
 <template>
-  <div class="container">
-    <h1>Omni Notification</h1>
-    <div>Show your notifications anywhere.</div>
+  <title-bar/>
+  <div class="playground">
+    <div class="actions">
+      <button type="submit" class="info" @click="$notify('info', 'This is an info message')">Info</button>
+      <button type="submit" class="success" @cllick="$notify('success', 'This is a success message')">Success</button>
+      <button type="submit" class="warning" @click="$notify('warning', 'This is a warning message')">Warning</button>
+      <button type="submit" class="error" @click="$notify('error', 'This is an error message')">Error</button>
+    </div>
+    <div class="options">
+
+    </div>
   </div>
 </template>
 
 <style scoped>
+.playground {
+  margin-top: 40px;
+  height: 50vh;
+}
+
+.actions {
+  display: flex;
+  gap: 10px;
+
+  button {
+    padding: 10px 20px;
+    width: 100px;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    font-size: 16px;
+    cursor: pointer;
+    box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.3);
+  } 
+  .info {
+    background-color: var(--color-info);
+  }
+  .success {
+    background-color: var(--color-success);
+  }
+  .warning {
+    background-color: var(--color-warning);
+  }
+  .error {
+    background-color: var(--color-error);
+  }
+}
 
 </style>
---
+++ examples/src/componets/TitleBar.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Omni Notification</h1>
+  <div class="description">Show your notifications anytime and anywhere.</div>
+</template>
+
+<style scoped>
+h1 {
+  color: var(--color-text-title);
+  font-size: 48px;
+}
+
+.description {
+  color: var(--color-text-subtitle);
+  font-size: 16px;
+  margin-bottom: 20px;
+}
+</style>
